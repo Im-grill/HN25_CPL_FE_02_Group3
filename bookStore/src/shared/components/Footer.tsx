90078f4f01e9c4a24a447e97ecd7907c4ac55e2a
Phạm Khuê <phamkhue98@gmail.com>
2025-03-31 22:35:58 +0700

    structure + ui footer

---
+++ bookStore/src/shared/components/Footer.tsx
@@ -0,0 +1,34 @@
+const Footer = () => {
+    return(
+        <footer className={'mt-16px'}>
+            <div>
+                <div>
+                    <div>
+                        <h4>Hỗ trợ khách hàng</h4>
+                        <p className={"hotline"}>
+                            Hotline:
+                            <a href="/">1900-6035</a>
+                            <span>(1000 đ/phút, 8-12h kể cả T7,CN)</span>
+                        </p>
+                        <a href={"/"}>Các câu hỏi thường gặp</a>
+                        <a href={"/"}>Gửi yêu cầu hỗ trợ</a>
+                        <a href={"/"}>Hướng dẫn đặt hàng</a>
+                        <a href={"/"}>Phương thức vận chuyển</a>
+                        <a href={"/"}>Chính sách kiểm hàng</a>
+                        <a href={"/"}>Chính sách đổi trả</a>
+                        <a href={"/"}>Hướng dẫn trả góp</a>
+                        <a href={"/"}>Chính sách hàng nhập khẩu</a>
+                        <a href={"/"}>Hỗ trợ khách hàng: hotro@tiki.vn</a>
+                        <a href={"/"}>Báo lỗi bảo mật: security@tiki.vn</a>
+                    </div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
