c8d2a002137efc5979ad781405873bc708835038
paulpham <phamkhue98@gmail.com>
2025-04-02 15:22:19 +0700

    update ui order page

--- bookStore/src/shared/layouts/CustomerLayout.tsx
+++ bookStore/src/shared/layouts/CustomerLayout.tsx
@@ -2,12 +2,14 @@ import { ReactNode } from "react";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 import { Outlet } from 'react-router-dom'
+import Footer from "../components/Footer.tsx";
 
 const CustomerLayout = () => {
     return (
         <div className="h-screen flex flex-col">
             <Header />
             <Sidebar />
+            <Footer/>
         </div>
     )
 }
