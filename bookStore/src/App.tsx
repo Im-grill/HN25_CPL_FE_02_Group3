038287cb4538cb8826c82678ab8a9b915fcde479
Im-grill <fakkukawaiiuwu@gmail.com>
2025-04-02 15:43:08 +0700

    Added Homepage and bootstrap

--- bookStore/src/App.tsx
+++ bookStore/src/App.tsx
@@ -1,5 +1,4 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/customer/views/homePage'
 import CustomerLayout from './shared/layouts/CustomerLayout'
@@ -8,39 +7,43 @@ import AdminLayout from './shared/layouts/AdminLayout'
 import Dashboard from './pages/admin/Dashboard'
 import CategoryList from './pages/admin/categoryManagement/CategoryList'
 import UserProvider from './shared/context/UserContext'
-import  { AlertProvider } from './shared/context/AlertContext'
+import {AlertProvider} from './shared/context/AlertContext'
 import Alert from './shared/components/admin/Alert'
 import UserProfile from './pages/customer/views/userProfile'
 import Confirm from './pages/customer/views/Confirm'
 import AddBook from './pages/admin/bookManagement/AddBook'
+import Order from './pages/customer/views/order'
+
 function App() {
 
 
-  return (
-    <BrowserRouter>
-      <UserProvider>
-        <AlertProvider>
-          <Alert />
-          <Routes>
-            {/* Customer */}
-            <Route path='/' element={<CustomerLayout />}>
-              <Route path="homepage" element={<HomePage />} />
-              <Route path="/productdetail" element={<ProductDetail />} />
-              <Route path="/userprofile" element={<UserProfile />} />
-              <Route path="confirm" element={<Confirm />} />
-            </Route>
+    return (
+        <BrowserRouter>
+            <UserProvider>
+                <AlertProvider>
+                    <Alert/>
+                    <Routes>
+                        {/* Customer */}
+                        <Route path='/' element={<CustomerLayout/>}>
+                            <Route path="homepage" element={<HomePage/>}/>
+                            <Route path="/productdetail" element={<ProductDetail/>}/>
+                            <Route path="/userprofile" element={<UserProfile/>}/>
+                            <Route path='order' element={<Order/>}></Route>
 
-            {/* Admin */}
-            <Route path='admin' element={<AdminLayout />}>
-              <Route index={true} element={<Dashboard />} />
-              <Route path='category' element={<CategoryList />} />
-              <Route path='add/book' element={<AddBook />} />
-            </Route>
-          </Routes>
-        </AlertProvider>
-      </UserProvider>
-    </BrowserRouter>
-  )
+                            <Route path="confirm" element={<Confirm/>}/>
+                        </Route>
+
+                        {/* Admin */}
+                        <Route path='admin' element={<AdminLayout/>}>
+                            <Route index={true} element={<Dashboard/>}/>
+                            <Route path='category' element={<CategoryList/>}/>
+                            <Route path='add/book' element={<AddBook/>}/>
+                        </Route>
+                    </Routes>
+                </AlertProvider>
+            </UserProvider>
+        </BrowserRouter>
+    )
 }
 
 export default App
