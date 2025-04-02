6ad9e4d9ebd872e9fa58cbe91c87c1a88661964e
Phạm Khuê <phamkhue98@gmail.com>
2025-04-02 18:25:58 +0700

    update routes order page

--- bookStore/src/App.tsx
+++ bookStore/src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import CustomerLayout from './shared/layouts/CustomerLayout'
 import RelSearchTopSeller from './shared/components/RelSearchTopSeller'
 import ProductDetail from './pages/admin/productManagement/productDetail'
+import Order from "./pages/customer/views/order.tsx";
+
 function App() {
   return (
     <BrowserRouter>
     <Routes>
     <Route path='/' element={<CustomerLayout />}></Route>
+    <Route path='order' element={<Order />}></Route>
     <Route path='/sell' element={<RelSearchTopSeller />} />
     <Route path="/productdetail" element={<ProductDetail />} />
     </Routes>
