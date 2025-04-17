887928a26ee5b04ff403318e952cc7fa2c80edcf
Im-grill <fakkukawaiiuwu@gmail.com>
2025-04-17 14:34:01 +0700

    fix bugs log in form in responsive

--- bookStore/src/shared/components/Header.tsx
+++ bookStore/src/shared/components/Header.tsx
@@ -4,20 +4,7 @@ import TikiImage from '../../assets/tiki-image.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Logo30Days from '../../assets/logo/img.png'
 import ArrowRight from '../../assets/logo/img_1.png'
-import {
-    faArrowLeft,
-    faBars,
-    faBox,
-    faCheckCircle,
-    faHome,
-    faMoneyBillTransfer,
-    faSearch,
-    faShoppingCart,
-    faTags,
-    faTruck,
-    faTruckFast,
-    faUser
-} from '@fortawesome/free-solid-svg-icons';
+import {faArrowLeft,faBars,faBox,faCheckCircle,faHome,faMoneyBillTransfer,faSearch,faShoppingCart,faTags,faTruck,faTruckFast,faUser} from '@fortawesome/free-solid-svg-icons';
 import { Link, useNavigate } from 'react-router-dom';
 import { IUser } from '../../interfaces/UserInterface';
 import { login as loginService, register as registerService } from '../../api/auth.service';
@@ -212,8 +199,81 @@ const Header = () => {
                         </div>
                     ) : (
                         <button onClick={() => setIsModalOpen(true)} className="text-gray-700 hover:text-blue-500">Tài
-                            khoản</button>
+                            khoản </button>
                     )}
+                </div>
+            )}
+             {isModalOpen && (
+                <div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-50 ">
+                    <div className="bg-white rounded-lg shadow-lg w-[800px] flex relative" style={{ bottom: '100px' }}>
+                        <button className="absolute top-4 left-4 text-gray-500 hover:text-black text-xl"
+                            onClick={() => setIsModalOpen(false)}>
+                            &nbsp;&lt;&nbsp;
+                        </button>
+
+                        <div className="w-1/2 p-6">
+                            <br />
+                            <h2 className="text-xl font-semibold mb-2">{isLogin ? 'Đăng nhập bằng email' : 'Tạo tài khoản'}</h2>
+                            <p className="text-sm mb-4">{isLogin ? 'Nhập email và mật khẩu tài khoản Tiki' : 'Đăng ký tài khoản để mua sắm'}</p>
+                            <form onSubmit={handleSubmit}>
+                                <div className="border-b-2 mb-4 border-gray-300">
+                                    <input type="email" placeholder="abc@email.com"
+                                        className="w-full py-2 border-none outline-none bg-transparent"
+                                        value={emailInput} onChange={(e) => setEmailInput(e.target.value)} required />
+                                </div>
+                                <div className="relative border-b-2 mb-4 border-gray-300">
+                                    <input type={showPassword ? 'text' : 'password'} placeholder="Mật khẩu"
+                                        className="w-full py-2 border-none outline-none bg-transparent pr-24"
+                                        value={password} onChange={(e) => setPassword(e.target.value)} required />
+                                    <button type="button"
+                                        className="absolute right-2 top-1/2 transform -translate-y-1/2 text-blue-500 cursor-pointer text-sm"
+                                        onClick={toggleShowPassword}>
+                                        {showPassword ? 'Ẩn' : 'Hiện'}
+                                    </button>
+                                </div>
+                                {!isLogin && (
+                                    <div className="border-b-2 mb-4 border-gray-300">
+                                        <input type="text" placeholder="Họ và tên"
+                                            className="w-full py-2 border-none outline-none bg-transparent"
+                                            value={fullNameInput}
+                                            onChange={(e) => setFullNameInput(e.target.value)} />
+                                    </div>
+                                )}
+                                {errorMessage && <p className="text-red-500 text-sm mb-2">{errorMessage}</p>}
+                                <button type="submit"
+                                    className={`w-full ${isLogin ? 'bg-red-500' : 'bg-blue-500'} text-white py-2 rounded`}>
+                                    {isLogin ? 'Đăng nhập' : 'Đăng ký'}
+                                </button>
+                            </form>
+                            <div className="flex justify-between text-sm mt-2">
+                                {isLogin ? (
+                                    <div className="flex flex-col space-y-2">
+                                        <a href="#" className="text-blue-500">Quên mật khẩu?</a>
+                                        <div className="flex items-center space-x-1">
+                                            <span className="text-gray-700 font-light">Chưa có tài khoản?</span>
+                                            <button onClick={() => setIsLogin(false)} className="text-blue-500">Tạo tài
+                                                khoản
+                                            </button>
+                                        </div>
+                                    </div>
+                                ) : (
+                                    <button onClick={() => setIsLogin(true)} className="text-blue-500">Đã có tài khoản?
+                                        Đăng nhập</button>
+                                )}
+                            </div>
+                        </div>
+
+                        <div
+                            className="w-1/2 bg-blue-50 flex flex-col items-center justify-center p-4 text-center relative">
+                            <img src={TikiImage} alt="Tiki" className="h-52 mb-2" />
+                            <p className="text-blue-500 font-semibold">Mua sắm tại Tiki</p>
+                            <p className="text-blue-500 text-sm">Siêu ưu đãi mỗi ngày</p>
+                            <button className="absolute top-4 right-4 text-gray-500 hover:text-black text-xl"
+                                onClick={() => setIsModalOpen(false)}>
+                                &#x2715;
+                            </button>
+                        </div>
+                    </div>
                 </div>
             )}
         </header>
@@ -237,6 +297,7 @@ const Header = () => {
                     <button
                         className="md:hidden text-gray-600 mr-3"
                         onClick={() => setIsMenuOpen(!isMenuOpen)}
+                        title='a'
                     >
                         <FontAwesomeIcon icon={faBars} className="h-5 w-5" />
                     </button>
@@ -359,32 +420,9 @@ const Header = () => {
                 </div>
             </div>
 
-
-            {/* Mobile menu */}
-            {isMenuOpen && (
-                <div className="md:hidden px-4 pb-4 space-y-2 bg-white">
-                    <Link to="/customer/homepage" className="block text-gray-700 hover:text-blue-500">Trang chủ</Link>
-
-                    {isLoggedIn ? (
-                        <div className="flex flex-col space-y-1">
-                            <div className="text-grey-700">{loggedInFullName}</div>
-                            {role === 'admin' && (
-                                <Link to="/admin" className="text-blue-500 hover:underline">Quản trị</Link>
-                            )}
-                            <button onClick={handleLogout} className="text-blue-500 hover:underline text-left">Đăng
-                                xuất
-                            </button>
-                        </div>
-                    ) : (
-                        <button onClick={() => setIsModalOpen(true)} className="text-gray-700 hover:text-blue-500">Tài
-                            khoản</button>
-                    )}
-                </div>
-            )}
-
             {/* Modal */}
             {isModalOpen && (
-                <div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-50">
+                <div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-50 ">
                     <div className="bg-white rounded-lg shadow-lg w-[800px] flex relative" style={{ bottom: '100px' }}>
                         <button className="absolute top-4 left-4 text-gray-500 hover:text-black text-xl"
                             onClick={() => setIsModalOpen(false)}>
